d3c3280cde792f63383a409d4abc008fc81b2e4b
demo <rajfernanto764@gmail.com>
2023-05-16 12:26:04 +0530

    first commit

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ['http://localhost:3000', 'https://rad-naiad-175e09.netlify.app/'],
+    origin: ['http://localhost:3000'],
     credentials: true,
   })
 );
